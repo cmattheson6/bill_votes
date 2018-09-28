487b2e0d7d37c10573dc1bf258efda8904c18d5d
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-09-28 09:17:41 -0400

    Update pipelines.py

--- senate_votes/pipelines.py
+++ senate_votes/pipelines.py
@@ -6,38 +6,63 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 ### -------- Import all of the necessary files -------- ###
-import psycopg2
+import scrapy
+from google.cloud import pubsub
+from google.oauth2 import service_account
+import subprocess
+import scrapy
+import scrapy.crawler
+from scrapy.utils.project import get_project_settings
+import google.auth
+
+### -------- Start of pipeline -------- ###
 
 class SenateVotesPipeline(object):
-    # Open database connection for uploading packets
-    def open_spider(self, spider):
-        hostname = 'localhost'
-        username = 'postgres'
-        password = 'postgres'
-        database = 'politics'
-        self.conn = psycopg2.connect(
-            host = hostname,
-            user = username,
-            password = password,
-            dbname = database)
-        self.cur = self.conn.cursor()
-
-    # Closes database for uploading packets
-    def close_spider(self, spider):
-        self.cur.close()
-        self.conn.close()
 
     def process_item(self, item, spider):
-        vote_packet = (item['bill_num'], 
-                       item['amendment_num'], 
-                       item['pol_id'], 
-                       item['vote_cast'], 
-                       item['vote_date'], 
-                       item['house'],
-                       item['state'])
-        insert_query = """insert into all_votes 
-            (bill_num, amdt_num, pol_id, vote_cast, vote_date, house, state)
-            values ({0}, {1}, {2}, {3}, {4}, {5}, {6})""".format(vote_packet)
-        self.cur.execute(insert_query)
-        self.conn.commit()
+        
+        cred_dict = {
+                         "auth_provider_x509_cert_url": spider.settings.get('auth_provider_x509_cert_url'),
+                         "auth_uri": spider.settings.get('auth_uri'),
+                         "client_email": spider.settings.get('client_email'),
+                         "client_id": spider.settings.get('client_id'),
+                         "client_x509_cert_url": spider.settings.get('client_x509_cert_url'),
+                         "private_key": spider.settings.get('private_key'),
+                         "private_key_id": spider.settings.get('private_key_id'),
+                         "project_id": spider.settings.get('project_id'),
+                         "token_uri": spider.settings.get('token_uri'),
+                         "type": spider.settings.get('account_type')
+             }
+        cred_dict['private_key'] = cred_dict['private_key'].replace('\\n', '\n')
+        print(cred_dict)
+
+        credentials = service_account.Credentials.from_service_account_info(cred_dict)
+        print(credentials)
+        print("I haven't set up the client yet, but I built the credentials!")
+        publisher = pubsub.PublisherClient(credentials = credentials)
+        print(publisher)
+        print("The client was set up!")
+
+        topic = 'projects/{project_id}/topics/{topic}'.format(
+             project_id='politics-data-tracker-1',
+             topic='bill_votes')
+        project_id = 'politics-data-tracker-1'
+        topic_name = 'bill_votes'
+        topic_path = publisher.topic_path(project_id, topic_name)
+        data = u'This is a representative in the House.'
+        data = data.encode('utf-8')
+        print("The topic was built!")
+        publisher.publish(topic_path, data=data,
+                          bill_id = str(item['bill_id']),
+                          amdt_id = str(item['amdt_id']),
+                          sponsor_ln = str(item['pol_ln']),
+                          sponsor_state = str(item['pol_state']),
+                          sponsor_party = str(item['pol_party']),
+                          vote_cast = str(item['vote_cast']),
+                          vote_date = str(item['vote_date']),
+                          house = str(item['house']),
+                          state = str(item['state']))
+        
+        print("We published! WOOOO!")
+        
         return item
