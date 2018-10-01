834e4a0ee93591a31d904f2dd3e5c5a2e629c2ff
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-09-30 22:37:52 -0400

    Update items.py

--- senate_votes/items.py
+++ senate_votes/items.py
@@ -11,8 +11,13 @@ import scrapy
 class ListOfVotes(scrapy.item.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    first_name = scrapy.Field()
-    last_name = scrapy.Field()
-    party = scrapy.Field()
+    bill_num = scrapy.Field()
+    amendment_num = scrapy.Field()
+    pol_fn = scrapy.Field()
+    pol_ln = scrapy.Field()
+    pol_party = scrapy.Field()
+    pol_state = scrapy.Field()
     vote_cast = scrapy.Field()
-
+    vote_date = scrapy.Field()
+    house = scrapy.Field()
+    state = scrapy.Field()
