df74539622f033911ab9d64af355b0138355bdab
cmattheson6 <42688579+cmattheson6@users.noreply.github.com>
2018-10-01 09:32:03 -0400

    Update senate_votes.py

--- senate_votes/spiders/senate_votes.py
+++ senate_votes/spiders/senate_votes.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+### ***NEED TO ADD IN CODE FOR FILTERING DATES
+
+
 ### -------- Import all of the necessary files -------- ###
 import scrapy
 from datetime import datetime
